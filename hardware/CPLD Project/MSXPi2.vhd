e920be3f690b817c91eab12f33a6ab007bb22e38
Ronivon Costa <ronivon.costa@gmail.com>
2020-07-04 00:18:11 +0100

    cpld pinout configured

--- hardware/CPLD Project/MSXPi2.vhd
+++ hardware/CPLD Project/MSXPi2.vhd
@@ -40,34 +40,37 @@ entity MSXPi2 is
 		WAIT_n : out std_logic;
 		
 		-- Raspberry Pi GPIO
-		GPIO_0 : in std_logic;
-		GPIO_1 : in std_logic;
-		GPIO_2 : in std_logic;
-		GPIO_3 : in std_logic;
-		GPIO_4 : in std_logic;
-		GPIO_5 : in std_logic;
-		GPIO_6 : in std_logic;
-		GPIO_7 : in std_logic;
-		GPIO_8 : in std_logic;
-		GPIO_9 : in std_logic;
-		GPIO_10 : in std_logic;
-		GPIO_11 : in std_logic;
-		GPIO_12 : in std_logic;
-		GPIO_13 : in std_logic;
-		GPIO_14 : in std_logic;
-		GPIO_15 : in std_logic;
-		GPIO_16 : in std_logic;
-		GPIO_17 : in std_logic;
-		GPIO_18 : in std_logic;
-		GPIO_19 : in std_logic;
-		GPIO_20 : in std_logic;
-		GPIO_21 : out std_logic;
-		GPIO_22 : in std_logic;
-		GPIO_23 : in std_logic;
-		GPIO_24 : in std_logic;
-		GPIO_25 : in std_logic;
-		GPIO_26 : in std_logic;
-		GPIO_27 : in std_logic
+		GPIO_D : inout std_logic_vector(7 downto 0);
+		GPIO_A : out std_logic_vector(13 downto 0);
+		--GPIO_D[0] : inout std_logic; -- D0 / GPIO_0
+		--GPIO_D[1] : inout std_logic; -- D1 / GPIO_1
+		--GPIO_D[2] : inout std_logic; -- D2 / GPIO_2
+		--GPIO_D[3] : inout std_logic; -- D3 / GPIO_3
+		--GPIO_D[4] : inout std_logic; -- D4 / GPIO_4
+		--GPIO_D[5] : inout std_logic; -- D5 / GPIO_5
+		--GPIO_D[6] : inout std_logic; -- D6 / GPIO_6
+		--GPIO_D[7] : inout std_logic; -- D7 / GPIO_7
+		--GPIO_A[0] : out std_logic; -- A0 / GPIO_8
+		--GPIO_A[1] : out std_logic; -- A1 / GPIO_9
+		--GPIO_A[2] : out std_logic; -- A2 / GPIO_10
+		--GPIO_A[3] : out std_logic; -- A3 / GPIO_11
+		--GPIO_A[4] : out std_logic; -- A4 / GPIO_12
+		--GPIO_A[5] : out std_logic; -- A5 / GPIO_13
+		--GPIO_A[6] : out std_logic; -- A6 / GPIO_14
+		--GPIO_A[7] : out std_logic; -- A7 / GPIO_15
+		--GPIO_A[8] : out std_logic; -- A8 / GPIO_16
+		--GPIO_A[9] : out std_logic; -- A9 / GPIO_17
+		--GPIO_A[10] : out std_logic; -- A10 / GPIO_18
+		--GPIO_A[11] : out std_logic; -- A11 / GPIO_19
+		--GPIO_A[12] : out std_logic; -- A12 / GPIO_20
+		--GPIO_A[13] : out std_logic; -- A13 / GPIO_22
+		
+		GPIO_21 : out std_logic; -- RPI CS signal - send interrupt ro RPi
+		GPIO_23 : out std_logic; -- MSX access type: 1 = read, 0 = write
+		GPIO_24 : out std_logic; -- MSX IO type: 1 = port access, 0 = memory access
+		GPIO_25 : in std_logic; -- RPI_ready
+		GPIO_26 : in std_logic; -- 
+		GPIO_27 : in std_logic  --
 	);
 
 end MSXPi2;
@@ -81,22 +84,13 @@ architecture ppl_type of MSXPi2 is
 begin
 
    D <= d_s;
-   GPIO_21 <= led_s;
-	WAIT_n <= 'Z';
+   
+	WAIT_n <= sltsl or GPIO_25;
 	BDIR <= 'Z';
 	
-	msxpi_en_s <= '1' when ((WR_n = '0' or RD_n = '0') and (IORQ_n = '0' or MREQ_n = '0')) else '0';
-	
-	d_s <= x"AA" when (A(7 downto 0)  = x"56")   and RD_n = '0' and  IORQ_n = '0' else
-	     x"BB" when (A(15 downto 0) = x"9876") and RD_n = '0' and  MREQ_n = '0' and SLTSL = '0' else
-		  x"CC" when (A(15 downto 0) = x"A876") and RD_n = '0' and  MREQ_n = '0' and SLTSL = '0' else
-		  (others => 'Z');
-	
-	led_s <= '1' when A(7 downto 0) = x"56" and WR_n = '0' and IORQ_n = '0' else
-	         '0' when A(7 downto 0) = x"57" and WR_n = '0' and IORQ_n = '0' else
-				'1' when (A(15 downto 0) = x"9876") and WR_n = '0' and  MREQ_n = '0' else
-				'0' when (A(15 downto 0) = x"A876") and RD_n = '0' and  MREQ_n = '0';
-
+	GPIO_23 <= not sltsl; =	-- RPI CS signal - send interrupt ro RPi
+	GPIO_23 <= WR_n;			-- 1 = read, 0 = write
+   GPIO_24 <= MREQ_n;		-- 1 = port access, 0 = memory access
 
 end;
 
